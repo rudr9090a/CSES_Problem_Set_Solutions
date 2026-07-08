13e636a66bb0a75319e089bb9cecfa5e2d177bfc
rudr9090a <rkmathe@gmail.com>
2026-07-09 01:12:42 +0530

    Apple Division

--- main.cpp
+++ main.cpp
@@ -1,48 +1,29 @@
 #include <bits/stdc++.h>
-#define range(i,a,b) for (int i = a; i <= b; i++)
-
+#define range(i,a,b) for (int i = a; i < b; i++)
 using namespace std;
-long long exponentiate_mod(long long a, long long b, long long mod) {
-    if (b == 0) {
-        return 1;
-    }
-    long long output = 1;
-    while(b>0) {
-        if (b%2 == 0) {
-            b = b/2;
-            a = (a*a)%mod;
-        }
-        else {
-            b -= 1;
-            output = (a*output)%mod;
-        }
-    }
-    return output;
-}
-long long triply_exponentiate(long long a, long long b, long long c,long long mod) {
-    long long exp;
-    exp = exponentiate_mod(b,c,mod-1);
-    long long final = exponentiate_mod(a,exp,mod);
-    return final;
-}
-
-
 int main() {
-    long long mod = 1e9 + 7;
-    int number_of_inputs;
-    cin >> number_of_inputs;
-    cin.ignore();
-    vector<vector<long long>> inputs;
-    vector<future<long long>> futures;
+    int n;
+    cin >> n;
+    vector<long long> inputs(n);
+    for (int i = 0; i < n; i++) {
+        cin >> inputs[i];
+    }
+    long long total = 0;
+    for (long long input:inputs) {
+        total += input;
+    }
+    long long ans = LLONG_MAX;
+    for (int mask = 0; mask < (1 << n); mask++) {
+        long long group1 = 0;
 
-    for (int i = 0; i < number_of_inputs; i++) {
-        string line;
-        getline(cin, line);
-        int a, b, c;
-        istringstream(line) >> a >> b >> c;
-        inputs.emplace_back(vector({static_cast<long long>(a),static_cast<long long>(b),static_cast<long long>(c)}));
+        for (int i = 0; i < n; i++) {
+            if (mask & (1 << i)) {
+                group1 += inputs[i];
+            }
+        }
+
+        long long group2 = total - group1;
+        ans = (ans<abs(group1 - group2)) ? ans : abs(group1 - group2);
     }
-    for (vector l : inputs) {
-        cout << triply_exponentiate(l[0], l[1], l[2], mod) << "\n";
-    }
+    cout << ans << endl;
 }
