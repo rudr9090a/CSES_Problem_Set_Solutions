ba365a3b3eee94d3787c58863ae1552b567e09f4
rudr9090a <rkmathe@gmail.com>
2026-07-09 17:16:47 +0530

    Ferris Wheel

--- main.cpp
+++ main.cpp
@@ -4,26 +4,24 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     //Solution
-    int n, m, adjustability;
-    cin >> n >> m >> adjustability;
-    vector<int> tenants(n), apartments(m);
-    for (int i = 0; i < n; i++) cin >> tenants[i];
-    for (int i = 0; i < m; i++) cin >> apartments[i];
-    sort(tenants.begin(), tenants.end());
-    sort(apartments.begin(), apartments.end());
-    int count_of_tenants_housed = 0,apartment_index = 0,tenant_index = 0;
-    while ((apartment_index < apartments.size()) && (tenant_index < tenants.size())) {
-        if (abs(apartments[apartment_index] - tenants[tenant_index]) <= adjustability) {
-            count_of_tenants_housed++;
-            tenant_index++;
-            apartment_index++;
-        } else if (apartments[apartment_index] > tenants[tenant_index]) {
-            tenant_index++;
-        } else if (apartments[apartment_index] < tenants[tenant_index]) {
-            apartment_index++;
+    int children, max_weight;
+    cin >> children >> max_weight;
+    vector<int> weights(children);
+    for (int i = 0; i < children; i++) cin >> weights[i];
+    sort(weights.begin(), weights.end());
+    int no_of_gondolas = 0;
+    int lo = 0;
+    int hi = children - 1;
+    while (lo<=hi) {
+        if (lo == hi) {
+            no_of_gondolas++;
+            break;
         }
+        if (weights[lo]+weights[hi] <= max_weight) {
+            lo++;
+        }
+        no_of_gondolas++;
+        hi--;
     }
-
-    cout << count_of_tenants_housed << "\n";
-
+    cout << no_of_gondolas << "\n";
 }
