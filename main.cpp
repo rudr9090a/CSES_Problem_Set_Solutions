203067093cfd63542cbd0f6ab101f89757e8773d
rudr9090a <rkmathe@gmail.com>
2026-07-10 21:04:01 +0530

    Weird Algorithm

--- main.cpp
+++ main.cpp
@@ -1,39 +1,7 @@
-#include <bits/stdc++.h>
-using namespace std;
-long long dice_combinations(int n, vector<long long>& memo) {
-    if (n == 0) {
-        return 1;
-    }
-    const long long MOD = 1000000007;
-    long long output=0;
-    output = memo[n];
-    if (output != -1) return output;
-    output = 0;
-    const vector dice = {1,2,3,4,5,6};
-    for (int possibility : dice) {
-        int remainder = n - possibility;
-        if (remainder >= 0) {
-            output += dice_combinations(remainder, memo)%MOD;
-        }
-    }
-    output %= MOD;
-    memo[n] = output;
-    return output;
-    }
-
-
+#include <iostream>
 int main() {
-
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    //Solution
-    const long long MOD = 1000000007;
-    vector<long long> memo(1000001,-1);
-    memo[0] = 1;
-    int number=0;
-    cin >> number;
-    long long output = 0;
-    output = dice_combinations(number,memo);
-    cout << output%MOD << endl;
-
+    long int a;
+    std::cin >> a;
+    std::cout << a <<  " ";
+    while (a > 1) {if (a%2 == 1) {std::cout << a*3 + 1 << " ";a= a*3 + 1;}else {std::cout << a/2 << " ";a = a/2;}}
 }
