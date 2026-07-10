9b9e3c02c77d27f0318d23eb9908d931546162b8
rudr9090a <rkmathe@gmail.com>
2026-07-10 21:06:54 +0530

    Permutations

--- main.cpp
+++ main.cpp
@@ -1,34 +1,66 @@
-
-#include <vector>
 #include <iostream>
 #include <string>
-#include <bits/stdc++.h>
 using namespace std;
+ 
 int main() {
-    vector<char> alphabets;
-    vector<int> numbers;
-    string input;
-    getline(cin, input);
-    char previous = input[0];
-    int count = 0 ;
-
-    for (int i = 0; i < input.length(); i++) {
-        if (previous == input[i]) {
-            count++;
-            alphabets.push_back(input[i]);
-            numbers.push_back(count);
-        }
-        else if (previous != input[i]) {
-            previous = input[i];
-            count = 1;
-
-        }
+    int inp;
+    cin >> inp;
+    if (inp == 1) {
+        cout << "1";
+        return 0;
     }
-    int largest = numbers[0];
-    for (int number : numbers) {
-        if (number > largest) {
-            largest = number;
-        }
+    if (inp == 4) {
+        cout << "2 4 1 3 ";
+        return 0;
     }
-    cout << largest << endl;
+    if (inp <= 3) {
+        cout << "NO SOLUTION" << endl;
+        return 0;
+    }
+ 
+    if (inp % 2 == 1) {
+        int breaker = (inp+1)/2;
+        int second_starter = breaker + 1;
+        int first_starter = 1;
+        bool odd = false;
+        string output = "";
+        for (int i = 1; i <= inp; ++i) {
+            if (!odd) {
+                odd = true;
+                output += (std::to_string(first_starter)+" ");
+                first_starter += 1;
+                continue;
+            }
+            if (odd) {
+                output += (std::to_string(second_starter)+" ");
+                second_starter += 1;
+                odd = false;
+ 
+            }
+        }
+        cout << output << endl;
+    } else {
+        int breaker = inp/2;
+        int first_starter = 1;
+        int second_starter = breaker+1;
+        bool odd = false;
+        string output = "";
+        for (int i = 1; i <= inp; ++i) {
+            if (!odd) {
+                odd = true;
+                output += (std::to_string(first_starter)+" ");
+                first_starter += 1;
+                continue;
+            }
+            if (odd) {
+                odd = false;
+                output += (std::to_string(second_starter)+" ");
+                second_starter += 1;
+ 
+            }
+        }
+        cout << output << endl;
+ 
+    }
+    return 0;
 }
