227d7fe2aada814cee8c86f9f110fbb648d22977
rudr9090a <rkmathe@gmail.com>
2026-07-10 21:04:41 +0530

    Missing Number

--- main.cpp
+++ main.cpp
@@ -1,7 +1,38 @@
+#include <vector>
 #include <iostream>
+#include <string>
+#include <bits/stdc++.h>
+using namespace std;
 int main() {
-    long int a;
-    std::cin >> a;
-    std::cout << a <<  " ";
-    while (a > 1) {if (a%2 == 1) {std::cout << a*3 + 1 << " ";a= a*3 + 1;}else {std::cout << a/2 << " ";a = a/2;}}
+    string ending_int_str;
+    getline(cin,ending_int_str);
+    int ending_int = stoi(ending_int_str);
+    string str;
+    getline(cin, str);
+    vector<int> vec;
+    str += " ";
+    string accumulated = "";
+    for (int  i = 0;i<=str.length();i++) {
+
+        if (str[i] == ' ') {
+            vec.push_back(stoi(accumulated));
+            accumulated = "";
+        }
+        else {
+            accumulated += str[i];
+        }
+    }
+    vector<int> ordered_list;
+    for (int i = 1;i<=ending_int;i++) {
+        ordered_list.push_back(i);
+    }
+    long int ordered_list_SUM = 0;
+    long int ved_SUM = 0;
+    for (int i = 0; i < ordered_list.size(); i++) {
+        ordered_list_SUM += ordered_list[i];
+    }
+    for (int i = 0; i < vec.size(); i++) {
+        ved_SUM += vec[i];
+    }
+    cout << ordered_list_SUM - ved_SUM << endl;
 }
