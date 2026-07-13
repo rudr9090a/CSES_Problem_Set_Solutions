555d292fb0496703902215c8951d844be51ea531
rudr9090a <rkmathe@gmail.com>
2026-07-13 17:12:13 +0530

    Playlist

--- main.cpp
+++ main.cpp
@@ -1,26 +1,19 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <map>
 using namespace std;
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int coins, money;
-    cin >> coins >> money;
-    vector<int> options(coins);
-    for (int i = 0; i < coins; i++) cin >> options[i];
-
-    vector<int> dp(money + 1, INT_MAX);
-    dp[0] = 0; // base case: 0 coins needed to make sum 0
-
-    for (int x = 1; x <= money; x++) {
-        for (int c : options) {
-            if (c <= x && dp[x - c] != INT_MAX) {
-                dp[x] = min(dp[x], dp[x - c] + 1);
-            }
-        }
+    int n;
+    cin >> n;
+    map<int, int> last;
+    int left = 0;
+    int best = 0;
+    for (int i = 1; i <= n; i++) {
+        int x;
+        cin >> x;
+        left = max(left, last[x]);
+        best = max(best, i - left);
+        last[x] = i;
     }
-
-    cout << (dp[money] == INT_MAX ? -1 : dp[money]) << "\n";
-    return 0;
+    cout << best << "\n";
 }
