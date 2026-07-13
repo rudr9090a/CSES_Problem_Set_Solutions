dc9e0548df39c9592bb3c36af2044b314893424e
rudr9090a <rkmathe@gmail.com>
2026-07-13 22:05:49 +0530

    Sum of two values

--- main.cpp
+++ main.cpp
@@ -1,19 +1,32 @@
-#include <iostream>
-#include <map>
+#include <bits/stdc++.h>
 using namespace std;
-
+typedef long long ll;
 int main() {
-    int n;
-    cin >> n;
-    map<int, int> last;
-    int left = 0;
-    int best = 0;
-    for (int i = 1; i <= n; i++) {
-        int x;
-        cin >> x;
-        left = max(left, last[x]);
-        best = max(best, i - left);
-        last[x] = i;
+    int n, x;
+    cin >> n >> x;
+    vector<pair<int,int>> v(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v[i].first;
+        v[i].second = i+1;
     }
-    cout << best << "\n";
+    sort(v.begin(), v.end());
+    auto p1 = v.begin();
+    auto p2 = v.end()-1;
+    while (p1->first <= p2->first) {
+
+        if (p1==p2) {
+            break;
+        }
+        if (p1->first + p2->first == x) {
+            cout << p1->second << " " << p2->second << endl;
+            return 0;
+        }
+        if (p1->first + p2->first > x) {
+            --p2;
+        } else {
+            ++p1;
+        }
+    }
+    cout << "IMPOSSIBLE" << "\n";
+    return 0;
 }
