8166edbb9a57ac121f34befdbd2cc108f1376285
rudr9090a <rkmathe@gmail.com>
2026-07-13 17:11:19 +0530

    Minimizing Coins

--- main.cpp
+++ main.cpp
@@ -1,43 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
 
+int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    //Solution
-    int customer=0;
-    cin >> customer;
-    vector<int> arrival_times(customer);
-    vector<int> departure_times(customer);
-    for (int i = 0; i < customer; i++) {
-        cin >> arrival_times[i] >> departure_times[i] ;
-    }
-    int person = 0;
-    sort(arrival_times.begin(), arrival_times.end());
-    sort(departure_times.begin(), departure_times.end());
-    auto i = arrival_times.begin();
-    auto j = departure_times.begin();
-    int p_max = 0;
-    while (j != departure_times.end()) {
-        if (i == arrival_times.end()) {
-            break;
-        }
-        if (*i < *j) {
-            person++;
-            p_max = (person > p_max) ? person : p_max;
-            auto end = arrival_times.end();
-            if (i == end) continue;
-            i++;
-        } else if (*i > *j) {
-            person--;
-            j++;
-        } else if (*i == *j) {
-            person--;
-            j++;
-            person++;
-            i++;
-            p_max = (person > p_max) ? person : p_max;
+
+    int coins, money;
+    cin >> coins >> money;
+    vector<int> options(coins);
+    for (int i = 0; i < coins; i++) cin >> options[i];
+
+    vector<int> dp(money + 1, INT_MAX);
+    dp[0] = 0; // base case: 0 coins needed to make sum 0
+
+    for (int x = 1; x <= money; x++) {
+        for (int c : options) {
+            if (c <= x && dp[x - c] != INT_MAX) {
+                dp[x] = min(dp[x], dp[x - c] + 1);
+            }
         }
     }
-    cout << p_max << endl;
+
+    cout << (dp[money] == INT_MAX ? -1 : dp[money]) << "\n";
+    return 0;
 }
